72c92e77a77d020137282a4b009dc9f49abc2518
Minhaj06 <milonmdminhajkobir@gmail.com>
2023-02-10 23:42:07 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = require("./src/routes/api");
+const app = new express();
+const path = require("path");
+require("dotenv").config();
+
+// Security Middlewares
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
+const morgan = require("morgan");
+
+// Database
+const mongoose = require("mongoose");
+
+// Security Middlewares Implement
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Rate Limiter
+const limiter = rateLimit({ windowMs: 15 * 60 * 100, max: 3000 });
+
+// DB Connetion
+const URI = "";
+const OPTION = { user: "", pass: "", autoIndex: true };
+mongoose.connect(URI, OPTION, (error) => {
+  console.log("DB Connected Successfully");
+  console.log(error);
+});
+
+// Routing Implement
+app.use("/api/v1", router);
+
+// Add REact Front End Routing
+app.use(express.static("client/build"));
+app.get("*", function (req, res) {
+  req.sendFile(path.resolve(--__dirname, "client", "build", "index.html"));
+});
+
+module.exports = app;
